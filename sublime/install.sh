fe66ad2a921f654aeb0d1a1bea6bdf4ca590a3d2
Faruk AYDIN <omerfaruk26@gmail.com>
2019-07-30 11:49:35 +0200

    Install sublime settings

---
+++ sublime/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s ~/dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User

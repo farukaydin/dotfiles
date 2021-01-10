ba2843b18d32eb32b96280e021fe2a72286f6d43
Faruk AYDIN <omerfaruk26@gmail.com>
2021-01-10 02:07:11 +0100

    Remove sublime settings

--- sublime/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-cp ~/dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
